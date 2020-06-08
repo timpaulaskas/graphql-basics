0890c2eec92d8fb4cc272c1b779d8a45510afd66
Tim Paulaskas <drookue@gmail.com>
2020-06-08 14:04:27 -0700

    Added graphql-yoga with hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world!')
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query!'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
