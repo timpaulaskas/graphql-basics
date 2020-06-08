b62255a5d2c75b7cbd78b24973ca61bb59331788
Tim Paulaskas <drookue@gmail.com>
2020-06-08 15:26:59 -0700

    Added demo data and query methods

--- src/index.js
+++ src/index.js
@@ -1,17 +1,99 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+// Scalar Types
+// String, Boolean, Int, Float, ID
+
+// Demo user data
+const users = [{
+    id: '1',
+    name: 'Tim',
+    email: 'tim@example.com',
+    age: 46
+}, {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}, {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com'
+}]
+
+// Demo post data
+const posts = [{
+    id: '10',
+    title: 'Title #',
+    body: '',
+    published: false
+}, {
+    id: '11',
+    title: 'GraphQL 101',
+    body: '',
+    published: true
+}, {
+    id: '12',
+    title: 'Title #',
+    body: 'GraphQL 201',
+    published: true
+}]
+
 // Type definitions
 const typeDefs = `
     type Query {
-        hello: String!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my first query!'
+        users(parent, args, ctx, info) {
+            if (!args.query) return users
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) return posts
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
+            })
+        },
+        me() {
+            return {
+                id: 'abc123',
+                name: 'Mike',
+                email: 'mike@example.com'
+            }
+        },
+        post() {
+            return {
+                id: '123abc',
+                title: 'Title of the post',
+                body: 'Content of the post',
+                published: false
+            }
         }
     }
 }
