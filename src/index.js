0d35cde9e16646c5ae54876fefac87d53b7dbb1a
Tim Paulaskas <drookue@gmail.com>
2020-06-09 16:39:44 -0700

    Added deleteUser mutation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {v4 as uuidv4} from 'uuid'
 // String, Boolean, Int, Float, ID
 
 // Demo user data
-const users = [{
+let users = [{
     id: '1',
     name: 'Tim',
     email: 'tim@example.com',
@@ -21,7 +21,7 @@ const users = [{
 }]
 
 // Demo post data
-const posts = [{
+let posts = [{
     id: '10',
     title: 'Title #',
     body: '',
@@ -42,7 +42,7 @@ const posts = [{
 }]
 
 // Demo comment data
-const comments = [{
+let comments = [{
     id: '100',
     author: '2',
     text: 'This is the first comment',
@@ -77,6 +77,7 @@ const typeDefs = `
 
     type Mutation {
         createUser(data: CreateUserInput!): User!
+        deleteUser(id: ID!): User!
         createPost(data: CreatePostInput!): Post!
         createComment(data: CreateCommentInput!): Comment!
     }
@@ -176,6 +177,23 @@ const resolvers = {
 
             users.push(user)
             return user
+        },
+        deleteUser(parent, args, ctx, info) {
+            const userIndex = users.findIndex((user) => user.id === args.id)
+            if (userIndex === -1) throw new Error('User not found')
+
+            const deleteUsers = users.splice(userIndex, 1)
+
+            posts = posts.filter((post) => {
+                const match = post.author === args.id
+                if (match) {
+                    comments = comments.filter((comment) => comment.post !== post.id)
+                }
+
+                return !match
+            })
+            comments = comments.filter((comment) => comment.author !== args.id)
+            return deleteUsers[0]
         },
         createPost(parent, args, ctx, info) {
             const userExists = users.some((user) => user.id === args.data.author)
